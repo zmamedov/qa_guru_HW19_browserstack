2685bfe06f9147ffaa8d2c7785af6be4a9b462a8
Zaur Mamedov <zaurmd1996@gmail.com>
2024-04-15 22:12:38 +0300

    Add second test of open article

--- tests/test_wikipedia.py
+++ tests/test_wikipedia.py
@@ -4,11 +4,25 @@ from selene import browser, have
 
 
 def test_search():
-    with step('Type search'):
+    with step('Search "Appium" articles.'):
         browser.element((AppiumBy.ACCESSIBILITY_ID, "Search Wikipedia")).click()
         browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type('Appium')
 
-    with step('Verify content found'):
+    with step('Verify content found.'):
         results = browser.all((AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title'))
         results.should(have.size_greater_than(0))
         results.first.should(have.text('Appium'))
+
+
+def test_open_first_article():
+    with step('Search "Python" articles.'):
+        browser.element((AppiumBy.ACCESSIBILITY_ID, "Search Wikipedia")).click()
+        browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type('Python')
+
+    with step('Verify content found.'):
+        results = browser.all((AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title'))
+        results.should(have.size_greater_than(0))
+        results.first.should(have.text('Python'))
+
+    with step('Click the first article.'):
+        browser.all((AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title')).first.click()
